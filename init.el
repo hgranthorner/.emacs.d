7b2462addba365c322aef9a34da3f2acc19c6d98
Grant <h.grant.horner@gmail.com>
2021-07-05 14:35:45 -0400

    Updated to straight

--- init.el
+++ init.el
@@ -1,21 +1,36 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
+(setq gc-cons-threshold (* 50 1000 1000))
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+      (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+        "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+        'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(straight-use-package 'use-package)
+
+(setq straight-use-package-by-default t)
 ;; Check operating system
 (setq is-mac (equal system-type 'darwin))
 (setq is-windows (equal system-type 'windows-nt))
 
 ;; Initialize use package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-when-compile
-  (require 'use-package))
-(setq use-package-always-ensure t)
+;; (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
+;; (eval-when-compile
+;;   (require 'use-package))
+;; (setq use-package-always-ensure t)
 
 (use-package diminish
   :init
@@ -173,7 +188,6 @@
 
 ;; yasnippet
 (use-package yasnippet
-  :ensure t
   :config
   (yas-global-mode t)
   (define-key yas-minor-mode-map (kbd "<tab>") nil)
@@ -230,6 +244,8 @@
 
 ;; Pug
 (use-package pug-mode)
+
+(setq gc-cons-threshold (* 2 1000 1000))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
