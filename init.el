3f6334dc60f59c54b836338821922d17ad2fae3c
Grant Horner <h.grant.horner@gmail.com>
2022-06-18 12:11:29 -0400

    initial commit

---
+++ init.el
@@ -0,0 +1,33 @@
+; set up melpa
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(setq package-archive-priorities '(("melpa" . 10)
+				   ("gnu" . 5)
+				   ("nongnu" . 2)))
+(package-initialize)
+
+; set custom settings
+(setq make-backup-files nil)
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(setq lisp-indent-function 'common-lisp-indent-function)
+(setq mac-command-modifier 'ctrl)
+(setq hg/packages '(exec-path-from-shell
+		    magit))
+
+; define custom functions
+(defun hg/sync-packages (packages)
+  (dolist (package hg/packages)
+    (when (not (package-installed-p package))
+      (package-install package))))
+
+; refresh and load packages
+(package-refresh-contents 'async)
+(hg/sync-packages hg/packages)
+
+; exec path from shell for mac
+(exec-path-from-shell-initialize)
+
+; not sure why I need this, but commit fails otherwise on m1 mac
+(setq global-git-commit-mode nil)
+
+; key bindings
